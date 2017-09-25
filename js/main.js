de5f94f8f62ff81f6bec791fc5c9c6e15d67029e
mbliok <mbliok@melontech.com>
2017-09-25 10:18:02 +0300

    Set up with handlebars,js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
+var cast = {
+    "characters": [
+        {
+            "name": "text 1",
+            "titleCode": "title 1",
+            "description": "desc 1"
+        },
+        {
+            "name": "text 2",
+            "titleCode": "title 2",
+            "description": "desc 2"
+        },
+        {
+            "name": "text 3",
+            "titleCode": "title 3",
+            "description": "desc 3"
+        }
+    ]
+}
+$(document).ready(function () {
+    // create referanxce to list template
+    var charactersTemplate = $('#character-template').html();
+    var compiledTemplate = Handlebars.compile(charactersTemplate);
+    $('.character-list-container').html(compiledTemplate(cast))
 
-$(document).ready(function() {
+    $('body').on('click', '.btn-hamburger--dark, .btn-hamburger--white', function () {
+
+        $(this).toggleClass('collapsed', 1000);
+        setTimeout(function () { $('.slide-menu').toggle() }, 500);
+
+    })
+
+    // var html = $("#template").html();
+    // var template = Handlebars.compile(html);
 
 
-
+    // $('.list').append(template({ item: item }));
 });
 
 
 
-
-$(window).resize(function() {
-  
-});
-
-$(window).trigger('resize');
